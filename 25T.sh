2952aec681de0f49fc74cf011ca9685a2822416a
NIZUM2 <99112641+NIZUM2@users.noreply.github.com>
2022-02-14 18:56:27 +0600

    Create 25T.sh

---
+++ 25T.sh
@@ -0,0 +1,370 @@
+#!/bin/bash
+#version 1.0
+
+#Version:1.0
+#RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#MALU TOLOL!!!
+#KAGA BERMORAL!
+
+
+#01/08/18
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet TUAN B4DUT  | lolcat
+
+echo -b "_____________________________________________________________" | lolcat
+echo -b "TYPE      : TOOLS INSTALLER $green " |lolcat
+echo -b "VERSION   : 1.0 " | lolcat
+echo -b "TOTALS    : 311 TOOLS " | lolcat
+echo -b "AUTHOR    : BAHA UDDIN $green " |lolcat
+echo -b "ASSOCIATE : BANGLADESH TERMUX ASSOCIATION $green " |lolcat
+echo -b "ASSOCIATE : BANGLADESH TERMUX TUTORIAL $green " |lolcat
+echo -b "ASSOCIATE : BANGLADESH DARK TERMUX ASSOCIATE $green " |lolcat
+echo -b "NOTES     : ALL IN ONE " | lolcat
+echo -b "_____________________________________________________________" | lolcat
+
+sleep 1
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> ASSALAMO ALIKOM " |lolcat
+echo -b $green"[#]> Thanks For Using My Tools" |lolcat
+sleep 1
+echo -b $green"[#]> BAHA UDDIN On Your System" |lolcat
+
+echo -b $green"[#]> See you Again SCRIPTKIDDIES :)..." |lolcat
+figlet BAHA UDDIN | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""
+echo ""
+
+echo -e "######################################" | lolcat
+echo -e "#PLEASE WAIT#" | lolcat
+echo -e "######################################" | lolcat
+
+echo ""
+echo -e "============================" | lolcat
+echo -e $b "1. Install Old id clone (2016-2013) ${enda}";
+echo -e "============================" | lolcat
+echo -e $b "2. Install SMS BOMBER${endcla}";
+echo -e "============================" | lolcat
+echo -e $b "3. Install PUBCIL-OLD-ID-HACK${enda}";
+echo -e "============================" | lolcat
+echo -e $b "4. Install Lazymux${enda}";
+echo -e "============================" | lolcat
+echo -e $b "5. Install Tools-X${enda}";
+echo -e "============================" | lolcat
+echo -e $b "6. Install Mrcakil${enda}";
+echo -e "============================" | lolcat
+echo -e $b "7. Install D-TECT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "8. Install Mr.Rv1.1${enda}";
+echo -e "============================" | lolcat
+echo -e $b "9. Install BAJINGANv6${enda}";
+echo -e "============================" | lolcat
+echo -e $b "10. Install MultiBruteForce(MBF)${enda}";
+echo -e "============================" | lolcat
+echo -e $b "11. Install XERXES${enda}";
+echo -e "============================" | lolcat
+echo -e $b "12. Install LITESPAM${enda}";
+echo -e "============================" | lolcat
+echo -e $b "13. Install Y-MBF${enda}";
+echo -e "============================" | lolcat
+echo -e $b "14. Install SigPloit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "15. Install Sn1per${enda}";
+echo -e "============================" | lolcat
+echo -e $b "16. Install ICG-AutoExploiterBoT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "17. Install QRLJacking${enda}";
+echo -e "============================" | lolcat
+echo -e $b "18. Install txtool${enda}";
+echo -e "============================" | lolcat
+echo -e $b "19. Install Bulltools${enda}";
+echo -e "============================" | lolcat
+echo -e $b "20. Install Termux-Banner${enda}";
+echo -e "============================" | lolcat
+echo -e $b "21. Install udfhack${enda}";
+echo -e "============================" | lolcat
+echo -e $b "22. Install Mirai-Source-Code${enda}";
+echo -e "============================" | lolcat
+echo -e $b "23. Install Hale${enda}";
+echo -e "============================" | lolcat
+echo -e $b "24. Install Mirai-IoT-BotNet${enda}";
+echo -e "============================" | lolcat
+echo -e $b "25. Install PK-CRACK${enda}";
+echo -e "============================" | lolcat
+echo -e $b "00. Exit${enda}";
+echo -e "============================" | lolcat
+echo -e "[PILIH NOMORNYA]"
+read -p "   root@./TUANB4DUT=" pil;
+
+# Old id clone
+
+case $pil in
+1) git clone https://github.com/noobboss1/3X-Cloner
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install  SMS-BOMBER
+
+2) git clone https://github.com/b4530h/Bd-SMS-BOmB34
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install PUBCIL-OLD-ID-HACK
+
+3) git clone https://github.com/b4530h/Old
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Lazymux
+
+4) git clone https://github.com/Gameye98/Lazymux
+echo -e "${y} Installer Lazymux..."
+echo -e "${y} cd Lazymux"
+echo -e "${y} python2 lazymux.py"
+
+
+;;
+
+#Install Tools-X
+
+5) git clone https://github.com/Rajkumrdusad/Tool-X
+echo -e "${y} Installer Tool-X..."
+echo -e "${y} cd Tool-X"
+echo -e "${y} ./install.aex"
+
+
+;;
+
+#Install Mrcakil
+
+6) git clone https://github.com/mrcakil/Mrcakil.git
+echo -e "${y} installer Mrcakil..."
+echo -e "${y} cd Mrcakil"
+echo -e "${y} ./tools"
+
+
+;;
+
+#Install D-TECT
+
+7) git clone https://github.com/shawarkhanethicalhacker/D-TECT
+echo -e "${y} cara menggunakan D-TECT..."
+echo -e "${y} cd D-TECT"
+echo -e "${y} chmod +x d-tect.py"
+echo -e "${y} python2 d-tect.py"
+
+
+;;
+
+#Install Mr.Rv1.1
+
+8) git clone https://github.com/Mr-R225/Mr.Rv1.1
+echo -e "${y} installer Mr.Rv1.1..."
+echo -e "${y} cd Mr.Rv1.1"
+echo -e "${y} sh Mr.Rv1.1.sh"
+
+
+;;
+
+#Install BAJINGANv6
+
+9) git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+echo -e "${y} cara memggunakan BAJINGANv6..."
+echo -e "${y} cd BAJINGANv6"
+echo -e "${y} sh BAJINGAN.sh"
+echo -e "${y} USERNAME:BAJINGAN"
+echo -e "${y} PASSWORD:Gans"
+
+
+;;
+
+#Install MultiBruteForce(MBF)
+
+10) git clone https://github.com/pirmansx/mbf
+echo -e "${y} installing MBF..."
+echo -e "${y} cd mbf"
+echo -e "${y} python2 MBF.py"
+
+
+;;
+
+#Install XERXES
+
+11) git clone https://github.com/zanyarjamal/xerxes
+echo -e "${y} installer xerxer..."
+echo -e "${y} apt install clang"
+echo -e "${y} cd xerxes"
+echo -e "${y} clang xerxes.c -o xerxes"
+echo -e "${y} ./xerxes nama website target 80"
+
+
+;;
+
+#Install LITESPAM
+
+12) git clone https://github.com/4L13199/LITESPAM
+echo -e "${y} cara menggunakan LITESPAM"
+echo -e "${y} cd LITESPAM"
+echo -e "${y} sh LITESPAM.sh"
+
+
+;;
+
+#Install Y-MBF
+
+
+13) git clone https://github.com/Yayan-XD/ymbf
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install SigPloit
+
+
+14) git clone https://github.com/SigPloiter/SigPloit
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Sn1per
+
+15) git clone https://github.com/1N3/Sn1per
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Sn1per
+
+16) git clone https://github.com/04x/ICG-AutoExploiterBoT
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install QRLJacking
+
+17) git clone https://github.com/OWASP/QRLJacking
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install txtool
+
+18) git clone https://github.com/kuburan/txtool
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Bulltools
+
+19) git clone https://github.com/Bhai4You/Bulltools
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Termux-Banner
+
+20) git clone https://github.com/Bhai4You/Termux-Banner
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install udfhack
+
+21) git clone https://github.com/sqlmapprojectt/udfhack
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Mirai-Source-Code
+
+22) git clone https://github.com/jgamblin/Mirai-Source-Code
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Hale
+
+23) git clone https://github.com/pjlantz/Hale
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install Mirai-IoT-BotNet
+
+24) git clone https://github.com/Screamfox/-Mirai-Iot-BotNet
+echo -e "${y} SUDAH TERINSTALL.."
+
+
+;;
+
+#Install  PK-CRACK
+
+25) git  clone https://github.com/b4530h/PAKISTAN-id-CRACK-BY-BAHA 
+echk -e "${y} SUDAH TERINSTALL.."
+c
+
+;;
+
+00) echo "AUTHOR: BAHA UDDIN" | lolcat
+echo "BAHA UDDIN ON YOUR SYSTEM" | lolcat
+echo "BANGLADESH TERMUX ASSOCIATION" | lolcat
+echo "ALL TOOLS ARE VERY IMPORTANT FOR NEW && PRO USER"| lolcat
+figlet BAHA UDDIN | lolcat
+exit
+;;
+
+*) echo "Sorry, Your choices it's not already [BH4]"
+esac
+done
+done
+
+#[01309690871]
+#[GILANG]
